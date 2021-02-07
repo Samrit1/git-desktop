248bc4fe324a059cca61d8a50a4d753da25f871f
Samrit1 <78609024+Samrit1@users.noreply.github.com>
2021-02-07 21:47:19 +0700

    up

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function loadLabeledImages() {
         labels.map(async label => {
             const descriptions = []
             for (let i = 1; i <= 5; i++) {
-                const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/Samrit1/git-desktop/main/labeled_images/${label}/${i}.jpg`)
+                const img = await faceapi.fetchImage(`https://githubusercontent.com/Samrit1/git-desktop/main/labeled_images/${label}/${i}.jpg`)
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
                 descriptions.push(detections.descriptor)
               }
