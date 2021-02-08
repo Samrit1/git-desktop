bc2dcc0a97e8c1948a7eec260c61fd7238fa8fd8
Samrit1 <78609024+Samrit1@users.noreply.github.com>
2021-02-08 15:29:58 +0700

    gg

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ async function start(){
 }
 
 function loadLabeledImages() {
-    const labels = ['Jisoo', 'Lisa', 'Rose', 'Tod']
+    const labels = ['Jisoo', 'Lisa', 'Rose', 'Jennie']
     return Promise.all(
         labels.map(async label => {
             const descriptions = []
